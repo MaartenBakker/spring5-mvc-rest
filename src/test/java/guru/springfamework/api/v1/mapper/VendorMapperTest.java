789b8c636b16eff31310b536f67ed862172b15bb
Maarten Bakker <maartenbakker@gmail.com>
2020-12-08 14:27:29 +0100

    Created VendorMapper Tests

---
+++ src/test/java/guru/springfamework/api/v1/mapper/VendorMapperTest.java
@@ -0,0 +1,40 @@
+package guru.springfamework.api.v1.mapper;
+
+import guru.springfamework.api.v1.model.VendorDTO;
+import guru.springfamework.domain.Vendor;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class VendorMapperTest {
+
+    public static final String NAME = "Vendor McVendorface";
+
+    public VendorMapper vendorMapper = VendorMapper.INSTANCE;
+
+    @Test
+    public void vendorToVendorDTO() {
+        //given
+        Vendor vendor = new Vendor();
+        vendor.setName(NAME);
+
+        //when
+        VendorDTO vendorDTO = vendorMapper.vendorToVendorDTO(vendor);
+
+        //then
+        assertEquals(vendor.getName(), vendorDTO.getName());
+    }
+
+    @Test
+    public void vendorDTOtoVendor() {
+        //given
+        VendorDTO vendorDTO = new VendorDTO();
+        vendorDTO.setName(NAME);
+
+        //when
+        Vendor vendor = vendorMapper.vendorDTOtoVendor(vendorDTO);
+
+        //then
+        assertEquals(vendorDTO.getName(), vendor.getName());
+    }
+}
