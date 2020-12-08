00e2e22a6b3438a16a25b5299c6280729dbd3cc8
Maarten Bakker <maartenbakker@gmail.com>
2020-12-08 14:42:08 +0100

    Created VendorServiceImpl testclass

---
+++ src/test/java/guru/springfamework/services/VendorServiceImplTest.java
@@ -0,0 +1,37 @@
+package guru.springfamework.services;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class VendorServiceImplTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void getAllVendors() {
+    }
+
+    @Test
+    public void getVendorById() {
+    }
+
+    @Test
+    public void createNewVendor() {
+    }
+
+    @Test
+    public void replaceVendorById() {
+    }
+
+    @Test
+    public void updateVendorById() {
+    }
+
+    @Test
+    public void deleteVendorById() {
+    }
+}
