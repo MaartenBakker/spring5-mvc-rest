57f654fa4d36a8dfc27ad640f14c53dc728aef98
Maarten Bakker <maartenbakker@gmail.com>
2020-12-08 14:41:11 +0100

    Created VendorServiceImpl without final implementation

---
+++ src/main/java/guru/springfamework/services/VendorServiceImpl.java
@@ -0,0 +1,38 @@
+package guru.springfamework.services;
+
+import guru.springfamework.api.v1.model.CustomerDTO;
+import guru.springfamework.api.v1.model.VendorDTO;
+
+import java.util.List;
+
+public class VendorServiceImpl implements VendorService {
+    @Override
+    public List<CustomerDTO> getAllVendors() {
+        return null;
+    }
+
+    @Override
+    public VendorDTO getVendorById(Long Id) {
+        return null;
+    }
+
+    @Override
+    public VendorDTO createNewVendor(VendorDTO vendorDTO) {
+        return null;
+    }
+
+    @Override
+    public VendorDTO replaceVendorById(Long Id, VendorDTO vendorDTO) {
+        return null;
+    }
+
+    @Override
+    public VendorDTO updateVendorById(Long Id, VendorDTO vendorDTO) {
+        return null;
+    }
+
+    @Override
+    public void deleteVendorById(Long Id) {
+
+    }
+}
