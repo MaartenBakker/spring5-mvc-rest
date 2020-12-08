706032ef6e762b863034e30706e062ff69fcd1cc
Maarten Bakker <maartenbakker@gmail.com>
2020-12-08 15:10:17 +0100

    Setup VendorController
    Fixed missing @Service at VendorServiceImpl

--- src/main/java/guru/springfamework/services/VendorServiceImpl.java
+++ src/main/java/guru/springfamework/services/VendorServiceImpl.java
@@ -4,10 +4,12 @@ import guru.springfamework.api.v1.mapper.VendorMapper;
 import guru.springfamework.api.v1.model.CustomerDTO;
 import guru.springfamework.api.v1.model.VendorDTO;
 import guru.springfamework.repositories.VendorRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Service
 public class VendorServiceImpl implements VendorService {
     public static final String VENDORS_URL = "/api/v1/vendors";
 
