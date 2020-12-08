acc71878798b855258c984a30eebe4889a3621dd
Maarten Bakker <maartenbakker@gmail.com>
2020-12-08 15:24:19 +0100

    Added and passed test for listVendors

---
+++ src/test/java/guru/springfamework/controllers/v1/VendorControllerTest.java
@@ -0,0 +1,60 @@
+package guru.springfamework.controllers.v1;
+
+import guru.springfamework.api.v1.model.VendorDTO;
+import guru.springfamework.api.v1.model.VendorListDTO;
+import guru.springfamework.controllers.RestResponseEntityExceptionHandler;
+import guru.springfamework.domain.Vendor;
+import guru.springfamework.services.VendorService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class VendorControllerTest {
+
+    @Mock
+    VendorService vendorService;
+
+    @InjectMocks
+    VendorController vendorController;
+
+    MockMvc mockMvc;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        mockMvc = MockMvcBuilders.standaloneSetup(vendorController)
+                .setControllerAdvice(new RestResponseEntityExceptionHandler())
+                .build();
+    }
+
+    @Test
+    public void listVendors() throws Exception {
+        //given
+        List<VendorDTO> vendorDTOS = Arrays.asList(new VendorDTO(),new VendorDTO());
+
+        when(vendorService.getAllVendors()).thenReturn(vendorDTOS);
+
+        //when/then
+        mockMvc.perform(get(VendorController.BASE_URL)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.vendors", hasSize(2)));
+
+    }
+}
